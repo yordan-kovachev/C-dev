931898f3e2d50bf1e062c874fd4e8c7501aec63f
unknown <yordan.kovachev@gmail.com>
2022-11-30 15:20:58 +0000

    13Task

--- 12Task/Program.cs
+++ 12Task/Program.cs
@@ -1,4 +1,10 @@
-﻿namespace _12Task
+﻿//Опишете разликите между C#, .NET Framework и .NET Core
+//
+//C# е програмен език, докато .NET Framework и .NET Core са платформи
+//за разработване изпълняване на .NET код, като по-новата е .NET Core
+//и тя работи върху Windows, Linux и macOS и е с отворен код
+
+namespace _12Task
 {
     internal class Program
     {
---
+++ 13Task/Program.cs
@@ -0,0 +1,29 @@
+﻿//Направете списък с най-популярните програмни езици. С какво те
+//се различават от C#?
+//
+// Answer
+//C#- C# is an object-oriented, component-oriented programming language
+//
+//JavaScript- to create dynamic and interactive web content like applications and browsers. JavaScript is so popular that it's the most used programming language in the world, used as a client-side programming language 
+//
+//Python- Python is a computer programming language often used to build websites and software, automate tasks, and conduct data analysis. Python is a general-purpose language, meaning it can be used to create a variety of different programs and isn't specialized for any specific problems
+//
+//HTML- HTML (HyperText Markup Language) is the code that is used to structure a web page and its content. For example, content could be structured within a set of paragraphs, a list of bulleted points, or using images and data tables.
+//
+//CSS- What is CSS? CSS is the language for describing the presentation of Web pages, including colors, layout, and fonts. It allows one to adapt the presentation to different types of devices, such as large screens, small screens, or printers. CSS is independent of HTML and can be used with any XML-based markup language.
+//
+//Java-What Does Java Mean? Java is an object-oriented programming language that produces software for multiple platforms
+//
+//SQL-SOL is a domain-specific synchronous programming language for developing distributed applications 
+//
+//NoSQL- NoSQL databases (aka "not only SQL") are non-tabular databases and store data differently than relational tables
+namespace _13Task
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
